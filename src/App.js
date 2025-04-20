d1891e8ebda8c2967c385dbe867c28527e5bbad3
danionpluto <dr676881@gmail.com>
2025-04-20 15:16:01 -0400

    homepage start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home';
+import Ex from './pages/explain';
+import Pj from './pages/project_teams';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+
+function Navbar () {
+  return (
+    <nav>
+      {/* <Link className = {"link-styles"} to="/">Branch Out</Link> */}
+      <Link className = {"link-styles2"} to="/">Home</Link>
+      <Link className = {"link-styles2"} to="/explain">How It Works</Link>
+      <Link className = {"link-styles2"} to="/project_teams">Project Teams</Link>
+      
+    </nav>
+   )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Navbar />
+          <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/explain" element={<Ex />} />
+              <Route path="/project_teams" element={<Pj />} />
+          </Routes>
+      </Router>
   );
 }
+
+
+
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,19 @@
+import React from "react";
+const Home = () => {
+    
+    return (
+
+        <div className="homepage" >
+            <div className="find-your">
+            <h1 className="home-title">Find Your Perfect Sustainability Project Team</h1>
+            <h3 className="home-explain">
+            Connect with Cornell project teams and clubs that align with your sustainability interests, skills, and passions. 
+            </h3>
+
+            
+            </div>
+        </div>
+    );
+};
+ 
+export default Home;
