498ae511d794102c393e6682eb8e11a0b50a1f0e
danionpluto <dr676881@gmail.com>
2025-04-20 16:43:01 -0400

    Finished Rest of Home Page

--- src/App.js
+++ src/App.js
@@ -3,30 +3,40 @@ import Home from './pages/home';
 import Ex from './pages/explain';
 import Pj from './pages/project_teams';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
 
 
-function Navbar () {
+function Navbar() {
   return (
     <nav>
       {/* <Link className = {"link-styles"} to="/">Branch Out</Link> */}
-      <Link className = {"link-styles2"} to="/">Home</Link>
-      <Link className = {"link-styles2"} to="/explain">How It Works</Link>
-      <Link className = {"link-styles2"} to="/project_teams">Project Teams</Link>
-      
+      <Link className={"link-styles2"} to="/">Home</Link>
+      {/* <Link className={"link-styles2"} to="/explain">How It Works</Link> */}
+      <ScrollLink
+        className={"link-styles2"}
+        to="how-it-works"
+        smooth={true}
+        duration={500}
+        offset={-70} // optional: offset if your navbar overlaps the section
+      >
+        How It Works
+      </ScrollLink>
+      <Link className={"link-styles2"} to="/project_teams">Project Teams</Link>
+
     </nav>
-   )
+  )
 }
 
 function App() {
   return (
-      <Router>
-          <Navbar />
-          <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/explain" element={<Ex />} />
-              <Route path="/project_teams" element={<Pj />} />
-          </Routes>
-      </Router>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        {/* <Route path="/explain" element={<Ex />} /> */}
+        <Route path="/project_teams" element={<Pj />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Pr from "/Users/danielaramos/Documents/GitHub/BranchOut/src/pics/community.jpeg";
 const Home = () => {
     
     return (
@@ -9,17 +10,56 @@ const Home = () => {
                 <h3 className="home-explain">
                 Connect with Cornell project teams and clubs that align with your sustainability interests, skills, and passions. 
                 </h3>
+                <button className="survey-nav-button">
+                    Take the Matching Survey!
+                </button>
             </div>
 
             <div className="why-use">
+                <h1 className="why-title">
+                    Why Use Cornell Sustainability Match?
+                </h1>
+
+                <div className="why-circles-container">
+
+                    <div className="why-circle">
+                        <img src= {Pr} alt="pr" className="circular-img"></img>
+                        <h3 className="why-circle-title">Personalized Recommendations</h3>
+                        <h6 className="why-circle-explain">Our algorithm matches you with project teams and clubs based on your unique interests, skills, and goals. </h6>
+
+                    </div>
+
+                    <div className="why-circle">
+                        <img src= {Pr} alt="pr" className="circular-img"></img>
+                        <h3 className="why-circle-title">Discover New Opportunities</h3>
+                        <h6 className="why-circle-explain">Explore sustainability initiatives across campus you might never have found on your own. </h6>
+
+                    </div>
+
+                    <div className="why-circle">
+                        <img src= {Pr} alt="pr" className="circular-img"></img>
+                        <h3 className="why-circle-title">Find Your Community</h3>
+                        <h6 className="why-circle-explain">Connect with like-minded peers passionate about making a difference in sustainability. </h6>
+
+                    </div>
+
+                </div>
 
             </div>
 
-            <div className="how-it-works">
-
+            <div id="how-it-works" className="how-it-works">
+                <h2>How It Works</h2>
+                <p>Lilly Code.</p>
             </div>
 
             <div className="ready">
+                <h1 className="home-title">Ready to find your match?</h1>
+                <h3 className="home-explain">
+                Take the survey now and discover project teams that align with your passions!
+                </h3>
+                <button className="survey-nav-button2">
+                    Take the Matching Survey!
+                </button>
 
             </div>
 
