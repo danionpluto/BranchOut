53c9d18bcc59a15bdb006147a27487a0231d6bf1
danionpluto <dr676881@gmail.com>
2025-04-20 15:19:45 -0400

    div organization

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,13 +5,25 @@ const Home = () => {
 
         <div className="homepage" >
             <div className="find-your">
-            <h1 className="home-title">Find Your Perfect Sustainability Project Team</h1>
-            <h3 className="home-explain">
-            Connect with Cornell project teams and clubs that align with your sustainability interests, skills, and passions. 
-            </h3>
-
-            
+                <h1 className="home-title">Find Your Perfect Sustainability Project Team</h1>
+                <h3 className="home-explain">
+                Connect with Cornell project teams and clubs that align with your sustainability interests, skills, and passions. 
+                </h3>
             </div>
+
+            <div className="why-use">
+
+            </div>
+
+            <div className="how-it-works">
+
+            </div>
+
+            <div className="ready">
+
+            </div>
+
+
         </div>
     );
 };
